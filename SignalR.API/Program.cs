ea0742b096d446035c38d2815099ca9f84384785
emrebuyuktas <emrebyk348@gmail.com>
2022-10-02 23:59:43 +0300

    create hub and establish connection

--- SignalR.API/Program.cs
+++ SignalR.API/Program.cs
@@ -1,8 +1,18 @@
+using SignalR.API.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("https://localhost:7286").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,9 +27,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
+app.UseEndpoints(enpoints =>
+{
+    enpoints.MapHub<MyHub>("/MyHub");
+    enpoints.MapControllers();
+});
 
-app.MapControllers();
 
 app.Run();
---
+++ SıgnalR.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SıgnalR.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
